6fbf3e22fbb43635aba209748f2b664d3c366775
moxizhou <mzhou8@u.rochester.edu>
2014-10-13 15:16:14 -0700

    Voice detection works in html, moved to the app.js

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,13 +2,68 @@
 angular.module('App', [])
 
 .controller('AppController', function ($scope, AppFactory) {
+  
   $scope.results = {};
+
   $scope.playMe = function(){
     AppFactory.translate($scope.results.translate).then(function(data) {
-      new GoogleTTS().play(data, "zh-CN", function() {
-        console.log("success");
-      });
+      console.log(data)
+      var msg = new SpeechSynthesisUtterance(data);
+      msg.lang = "zh-CN";
+      speechSynthesis.speak(msg);
     });
+
+  $scope.voice = function() {
+     var finalTranscript = '';
+          var recognizing = false;
+          var ignore_onend;
+
+          if('webkitSpeechRecognition' in window) {
+            var recognition = new webkitSpeechRecognition();
+            recognition.continuous = true;
+            recognition.interimResults = true;
+
+            recognition.onstart = function(event) {
+              recognizing = true;
+              console.log("speaking");
+            }
+
+            recognition.onerror = function(event) {
+              ignore_onend = true;
+              console.log(event.error);
+            }
+
+            recognition.onend = function() {
+              recognizing = false;
+              if (ignore_onend) {
+                return;
+              }
+              console.log("stopped")
+            }
+
+            recognition.onresult = function(event) {
+              var interimTranscript = '';
+              for (var i = event.resultIndex; i<event.results.length; i++) {
+                if (event.results[i].isFinal) {
+                  finalTranscript += event.results[i][0].transcript;
+                } else {
+                  interimTranscript += event.results[i][0].transcript;
+                }
+              }
+              console.log(finalTranscript);
+            }
+          };
+
+          
+            if (recognizing) {
+              recognition.stop();
+              return;
+            } 
+            finalTranscript = '';
+            recognition.lang = 'en-US';                
+            recognition.start();
+          
+  }
   }
 })
 
