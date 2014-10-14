48c841c927895fc103cb0dfef4180ee6770fee96
moxizhou <mzhou8@u.rochester.edu>
2014-10-14 11:08:06 -0700

    Bound dialect select

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,12 +2,117 @@
 angular.module('App', [])
 
 .controller('AppController', function ($scope, AppFactory) {
-
   var languages = {};
+  $scope.dialects = undefined;
 
-  // $scope.updateCountry = function(){
-  //   var list = langs[selectedLanguage.selectedIndex];
-  // };
+  $scope.langs2 = [
+ {name:'Afrikaans',       code:['af-ZA']},
+ {name:'Bahasa Indonesia',code:['id-ID']},
+ {name:'Bahasa Melayu',   code:['ms-MY']},
+ {name:'Català',          code:['ca-ES']},
+ {name:'Čeština',         code:['cs-CZ']},
+ {name:'Deutsch',         code:['de-DE']},
+ {name:'English',         code:['en-AU', 'Australia',
+                                'en-CA', 'Canada',
+                                'en-IN', 'India',
+                                'en-NZ', 'New Zealand',
+                                'en-ZA', 'South Africa',
+                                'en-GB', 'United Kingdom',
+                                'en-US', 'United States']},
+ {name:'Español',         code: ['es-AR', 'Argentina',
+                                'es-BO', 'Bolivia',
+                                 'es-CL', 'Chile',
+                                 'es-CO', 'Colombia',
+                                 'es-CR', 'Costa Rica',
+                                 'es-EC', 'Ecuador',
+                                 'es-SV', 'El Salvador',
+                                 'es-ES', 'España',
+                                 'es-US', 'Estados Unidos',
+                                 'es-GT', 'Guatemala',
+                                 'es-HN', 'Honduras',
+                                 'es-MX', 'México',
+                                 'es-NI', 'Nicaragua',
+                                 'es-PA', 'Panamá',
+                                 'es-PY', 'Paraguay',
+                                 'es-PE', 'Perú',
+                                 'es-PR', 'Puerto Rico',
+                                 'es-DO', 'República Dominicana',
+                                 'es-UY', 'Uruguay',
+                                 'es-VE', 'Venezuela']},
+ {name:'Euskara',           code: ['eu-ES']},
+ {name:'Français',          code: ['fr-FR']},
+ {name:'Galego',            code: ['gl-ES']},
+ {name:'Hrvatski',          code: ['hr_HR']},
+ {name:'IsiZulu',           code: ['zu-ZA']},
+ {name:'Íslenska',          code: ['is-IS']},
+ {name:'Italiano',          code: ['it-IT', 'Italia',
+                                  'it-CH', 'Svizzera']},
+ {name:'Magyar',            code: ['hu-HU']},
+ {name:'Nederlands',        code: ['nl-NL']},
+ {name:'Norsk bokmål',      code: ['nb-NO']},
+ {name:'Polski',            code: ['pl-PL']},
+ {name:'Português',         code: ['pt-BR', 'Brasil',
+                                  'pt-PT', 'Portugal']},
+ {name:'Română',            code: ['ro-RO']},
+ {name:'Slovenčina',        code: ['sk-SK']},
+ {name:'Suomi',             code: ['fi-FI']},
+ {name:'Svenska',           code: ['sv-SE']},
+ {name:'Türkçe',            code: ['tr-TR']},
+ {name:'български',         code: ['bg-BG']},
+ {name:'Pусский',           code: ['ru-RU']},
+ {name:'Српски',            code: ['sr-RS']},
+ {name:'한국어',              code: ['ko-KR']},
+ {name:'中文',               code: ['cmn-Hans-CN', '普通话 (中国大陆)',
+                                  'cmn-Hans-HK', '普通话 (香港)',
+                                  'cmn-Hant-TW', '中文 (台灣)',
+                                  'yue-Hant-HK', '粵語 (香港)']},
+ {name: '日本語',            code: ['ja-JP']},
+ {name: 'Lingua latīna',    code: ['la']}
+ ];
+
+ $scope.updateCountry = function() {
+    for (var i = 0; i < langs2.length ; i++) {
+      if ($scope.languages.select.name === (langs2[i][0])) {
+        var test = langs2[i].slice(1)
+        $scope.dialects = test
+        }
+      }
+  };
+
+ $scope.outlangs = [
+ {name:'Afrikaans',       code:'af'},
+ {name:'Indonesian',      code:'id'},
+ {name:'Bahasa Melayu',   code:'ms'},
+ {name:'Català',          code:'ca'},
+ {name:'Čeština',         code:'cs'},
+ {name:'Deutsch',         code:'de'},
+ {name:'English',         code:'en'},
+ {name:'Spanish',         code:'es'}, 
+ {name:'Euskara',         code:'eu'},
+ {name:'French',          code:'fr'},
+ {name:'Galego',          code:'gl'},
+ {name:'Hrvatski',        code:'hr'},
+ {name:'Italiano',        code:'it'},
+ {name:'Magyar',          code:'hu'},
+ {name:'Hindi',           code:'hi'},
+ {name:'Nederlands',      code:'nl'},
+ {name:'Polski',          code:'pl'},
+ {name:'Português-Bra',   code:'pt'},
+ {name:'Portugues-Por',   code:'pt'},
+ {name:'Română',          code:'ro'},
+ {name:'Slovenčina',      code:'sk'},
+ {name:'Suomi',           code:'fi'},
+ {name:'Svenska',         code:'sv'},
+ {name:'Türkçe',          code:'tr'},
+ {name:'български',       code:'bg'},
+ {name:'Pусский',         code:'ru'},
+ {name:'Српски',          code:'sr'},
+ {name:'한국어',            code:'ko'},
+ {name:'中文 (中国大陆)',   code:'zh-CN'},
+ {name:'中文 (台灣)',       code:'zh-CN'},
+ {name:'日本語',           code:'ja'},
+ {name:'Lingua latīna',   code:'la'}
+ ];
 
   $scope.voice = function() {
     var finalTranscript = '';
@@ -45,8 +150,8 @@ angular.module('App', [])
           }
         }
 
-        var source = AppFactory.source($scope.languages.select);
-        var target = $scope.languages.output;
+        var source = AppFactory.source($scope.languages.select.code);
+        var target = $scope.languages.output.code;
         console.log("final transcript", finalTranscript, "and source", source);
         
         AppFactory.translate(finalTranscript, source, target).then(function(data) {
@@ -56,6 +161,7 @@ angular.module('App', [])
         speechSynthesis.speak(msg);
         });
       }
+
     };
 
     if (recognizing) {
@@ -64,7 +170,7 @@ angular.module('App', [])
     } 
 
     finalTranscript = '';
-    recognition.lang = $scope.languages.select; 
+    recognition.lang = $scope.languages.select.code; 
     recognition.start();
   }
 })
@@ -81,6 +187,7 @@ angular.module('App', [])
   };
 
   var source = function(input) {
+    console.log(input)
     if (input === 'cmn-Hans-CN') {
       return "zh-CN";
     } else if (input === 'yue-Hant-HK' || input ==='ccmn-Hans-HK' || input ==='cmn-Hant-TW') {
@@ -97,9 +204,8 @@ angular.module('App', [])
 
 })
 
-// .factory('ButtonFactory', function($resource) {
-//   return $resource('/app/langs.js');
-// });
+
+
 
 
 
